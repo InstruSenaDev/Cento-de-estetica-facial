e7251261789936625f96cdeb70ce1c6bca172ba1
Nata-Sd <159500898+Nata-Sd@users.noreply.github.com>
2024-09-07 10:35:26 -0500

    error 404

---
+++ FrontEnd/src/componentes/Error404/Errorderuta.jsx
@@ -0,0 +1,37 @@
+import react from 'react';
+import './Error404.css';
+
+
+
+export const Error404= () => {
+  
+    return (
+	
+	<div classname="room">
+		<div classname="cuboid">
+			<div classname="side"></div>
+			<div classname="side"></div>
+			<div classname="side"></div>
+	</div>
+
+	<div classname="oops">
+		<h2>OOPS!</h2>
+		<p>No podemos encontrar la página que estás buscando. :(</p>
+	</div>
+
+	<div classname="center-line">
+		<div classname="hole">
+			<div classname="ladder-shadow"></div>
+			<div classname="ladder"></div>
+		</div>
+		
+		<div classname="four">4</div>
+		<div classname="four">4</div>
+		<div classname="btn">
+			<a href="#">Vuelve al Inicio</a>
+			</div>
+	</div>
+</div>
+
+    )
+}
---
+++ FrontEnd/src/pages/Error404.jsx
@@ -0,0 +1,13 @@
+import React, { useContext, useState } from "react";
+import {Error404} from "../componentes/Error404/Errorderuta"
+
+export const VolverHome = () => {
+
+  return (
+    <>
+   <Error404/>
+    </>
+  );
+};
+
+export default VolverHome;
